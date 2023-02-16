7c0332cecdb1d7a661934ed6787c54ae2ed0c41b
helenfaith <ejembichenenyefaith1994@gmail.com>
2023-02-16 10:52:06 +0100

    works

---
+++ Javascript/class work/calculator/index.js
@@ -0,0 +1,7 @@
+function calculate(){
+    var num1= document.getElementById("num1").value
+    var num2= document.getElementById("num2").value
+    var sum= number(num1) + number(num2)
+    document.getElementById("answer").innerHTML = sum
+    
+}
